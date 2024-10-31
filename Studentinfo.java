52f7ca1db9908c67e277c2e9197c42d35ccce6e0
stanleylogan70 <stanleylogan70@gmail.com>
2024-10-31 16:13:42 -0400

    first commit

---
+++ Studentinfo.java
@@ -0,0 +1,15 @@
+package studentinfo;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        // Variables to store the details
+        String name = "Logan Stanley";
+        String major = "Information Systems";
+        String firstJobLocation = "IT specialist at Public Debt";
+
+        // Output the details
+        System.out.println("Name: " + name);
+        System.out.println("Major: " + major);
+        System.out.println("Expected First Job Location: " + firstJobLocation);
+    }
+}
